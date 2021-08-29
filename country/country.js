1bfbfe460e3c0c47a28fb379e9d13fdadc663828
mesu-ai <mesu.s.ss@gmail.com>
2021-08-29 23:35:08 +0600

    country

--- country/country.js
+++ country/country.js
@@ -10,25 +10,48 @@ cosnt = displayCountry = (data) => {
 
     data.forEach(country => {
 
-        // console.log(country);
+        console.log(country);
 
         const div = document.createElement('div');
         div.classList.add('country');
 
-        const h3 = document.createElement('h3');
-        h3.innerText = country.name;
+        div.innerHTML = `
+        <h3> Country: ${country.name} </h3>
+        <p> Capital: ${country.capital}</p>
+        
+        <button onclick="counterDetails('${country.name}')"> Details </button>
 
-        const p = document.createElement('p');
-        p.innerHTML = ` Capital: ${country.capital}, Border: ${country.borders}`;
-
-
-        div.appendChild(h3);
-        div.appendChild(p);
-
+        `
         showCountry.appendChild(div);
 
-
     });
+
+
+}
+
+const counterDetails = (name) => {
+    const url = `https://restcountries.eu/rest/v2/name/${name}`;
+
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayDetails(data[0]));
+
+}
+
+const displayDetails = (data) => {
+    console.log(data.name);
+
+    const details = document.getElementById('details-Id');
+
+    const div = document.createElement('div');
+    div.innerHTML = `
+    <h3>Name:${data.name}</h3>
+    <p>Name:${data.population}</p>
+    <p>Name:${data.borders}</p>
+    <img src="${data.flag}">
+
+    `
+    details.appendChild(div);
 
 
 
