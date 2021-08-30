44b74c46fa1039b46838ad1fae0c9620fa1d49c7
mesu-ai <mesu.s.ss@gmail.com>
2021-08-30 16:39:23 +0600

    food search

--- food/mealdb.js
+++ food/mealdb.js
@@ -3,42 +3,84 @@
 const search = () => {
     const inputField = document.getElementById('input-field');
     const inputText = inputField.value;
-    inputField.value = '';
+
+    if (inputField.value == '') {
+        console.log('Enter Meal Name !');
+
+    } else {
+
+        inputField.value = '';
+        const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${inputText}`;
+        fetch(url)
+            .then(res => res.json())
+            .then(data => searchData(data.meals));
 
 
-    // console.log(inputText);
 
-    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${inputText}`
-    fetch(url)
-        .then(res => res.json())
-        .then(data => searchData(data.meals));
+    }
 
 }
 
 const searchData = (meals) => {
+    // if (meals.length == 0) {
+    //     console.log('ok');
+
+    // }
+    const mealShow = document.getElementById('meal-show');
+    mealShow.textContent = '';
+
 
     meals.forEach(meal => {
-
-        const mealShow = document.getElementById('meal-show');
-
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
-           <div class="card h-100 shadow">
+           <div onclick="mealDetails(${meal.idMeal})" class="card h-100 shadow">
                 <img class="card-img-top p-3" src=" ${meal.strMealThumb} " alt="">
                 <div class="card-body">
                     <h4 class="card-title">${meal.strMeal} </h4>
-                    <p class="card-text">${meal.strInstructions}</p>
+                    <p class="card-text">${meal.strInstructions.slice(0, 150)}</p>
                 </div>
             </div>
         `;
 
-
         mealShow.appendChild(div);
-
         console.log(meal);
 
     });
 
+}
 
+const mealDetails = (mealId) => {
+
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`;
+
+    fetch(url)
+        .then(res => res.json())
+        .then(data => showDetails(data.meals[0]));
+
+
+}
+
+const showDetails = (meal) => {
+    const details = document.getElementById('details-show');
+    details.textContent = '';
+
+    const div = document.createElement('div');
+    div.classList.add('col');
+
+    div.innerHTML = `
+    <hr>
+    <h3 class="text-black text-center my-3 fw-bold">Meal Details</h3>
+           <div class="card shadow">
+                <img src="${meal.strMealThumb}" class="card-img-top p-3" alt="">
+                <div class="card-body">
+                    <h5 class="card-title">${meal.strMeal}</h5>
+                    <p class="card-text">${meal.strInstructions}</p>
+                </div>
+            </div>
+            <hr>
+    
+    `;
+
+    details.appendChild(div);
 }
